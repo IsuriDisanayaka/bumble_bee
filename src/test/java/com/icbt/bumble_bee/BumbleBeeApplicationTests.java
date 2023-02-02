fef75dc34fc2f159462f281b23aa8de0122231d3
Isuri Disanayaka <isuriumeshika1@gmail.com>
2023-02-02 18:05:33 +0530

    Initial commit

---
+++ src/test/java/com/icbt/bumble_bee/BumbleBeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icbt.bumble_bee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumbleBeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
